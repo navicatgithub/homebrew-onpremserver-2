0b6e2ce73394759cea93a3144e1ad8eea4c94699
Navicat <github@navicat.com>
2024-12-16 14:16:29 +0800

    Update navicatonpremserver to v2.0.4

--- navicatonpremserver2.rb
+++ navicatonpremserver2.rb
@@ -1,10 +1,10 @@
 class Navicatonpremserver2 < Formula
-  version "2.0.3"
+  version "2.0.4"
   desc "Navicat On-Prem Server is an on-premise solution that provides you with the option to host a cloud environment for storing Navicat objects internally at your location. You can enjoy complete control over your system and maintain 100% privacy."
   homepage "https://www.navicat.com/en/products/navicat-on-prem-server"
-  url "https://download3.navicat.com/onpremsvr2-download/homebrew/navicat-onprem-server-2.0.3.tar.gz"
-  mirror "https://dn.navicat.com.cn/onpremsvr2-download/homebrew/navicat-onprem-server-2.0.3.tar.gz"
-  sha256 "fffda69c2143deb3905cdbcb679e5f87317f345bae8bd12458953c77089a8128"
+  url "https://download3.navicat.com/onpremsvr2-download/homebrew/navicat-onprem-server-2.0.4.tar.gz"
+  mirror "https://dn.navicat.com.cn/onpremsvr2-download/homebrew/navicat-onprem-server-2.0.4.tar.gz"
+  sha256 "855723309172325e2bf71edcc1badc2aed75b0172284756b1e75266496cac9fa"
 
   def install
     # Preload
